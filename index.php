22a077553fe67200af9b7eb154f5bfdd4501b15b
Naseath Saly <naseath@web-essentials.asia>
2016-09-27 14:45:51 +0700

    [TASK] Initiate winner random value

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head><title>FIND WINNER</title></head>
+
+<table >
+ 	<tr><td colpand="10" ><h1>FIND WINNER</h1></td></tr>
+	<tr colspand="2">
+	<td width="500px">
+		<?php
+echo file_get_contents( "name.txt"  );
+?>
+	</td>
+	<td valign="top" align="left"><a href="http://winner.wehost.asia/random.php"><h2>FIND</a>
+	<br/><br/>
+		-------------
+</h2>
+	</td>
+	</tr>
+</table>
+</html>
---
+++ random.php
@@ -0,0 +1,24 @@
+<html>
+<head><title>FIND WINNER</title></head>
+
+<table >
+ 	<tr><td colpand="10" ><h1>FIND WINNER</h1></td></tr>
+	<tr colspand="2">
+	<td width="500px">
+		<?php
+echo file_get_contents( "name.txt"  );
+?>
+	</td>
+	<td valign="top" align="left"><a href="http://winner.wehost.asia/random.php"><h2>FIND AGAIN</a>
+	<br/><br/>
+		<?php
+$lines = file('name.txt');
+
+echo $lines[array_rand($lines)];
+?>:-)
+</h2>
+	</td>
+	</tr>
+	<tr><td valign="top" align="left"><a href="http://winner.wehost.asia">BACK</a></td></tr>
+</table>
+</html>
